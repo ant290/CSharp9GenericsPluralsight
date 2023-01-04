6051b074b698c1f2e808cde7e10010a40981ada0
Anthony Parker-Perry <anthony.parker-perry@datasym.co.uk>
2023-01-04 17:17:27 +0000

    Simple Stack Implementation

---
+++ WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var stack = new SimpleStack();
+stack.Push(1.2);
+stack.Push(2.8);
+stack.Push(3.0);
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    var item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
+
+Console.WriteLine($"Sum: {sum}");
+Console.ReadLine();
